e6bd3161611744bea5bcfbea73aa67a31bdfaa8c
Abhishek A B <bsheks@gmail.com>
2025-10-04 20:56:48 +0530

    Commiting the modified game board component. Added the X on click feature.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,12 +7,23 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard(){
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex){
+        /*Deep copying the existing game board and then updating it based on the clicks*/
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = 'X';
+            return updatedBoard;
+        });
+    }
+    /* Creating a 2 dimensional list of the elements in the game board. */ 
     return(
         <ol id="game-board">
-            {initialGameBoard.map((row,rowIndex) => (<li key={rowIndex}>
+            {gameBoard.map((row,rowIndex) => (<li key={rowIndex}>
                 <ol>
                     {row.map((playerSymbol, colIndex) => (<li key={colIndex}>
-                        <button>{playerSymbol}</button>
+                        <button onClick={() => handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
                     </li>))}
                 </ol>
             </li>))}
